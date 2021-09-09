710fa9d36f6c4ea9b613485454b9398dd6e67f22
Daud <daudmangaraja@gmail.com>
2021-09-09 23:23:25 +0700

    Updates

---
+++ precache-manifest.c5892d1cf081ff9d21a787172a141112.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af21ce34422008df81e9eebc6ae3159b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a436739529af887307e0",
+    "url": "/static/css/2.7285519e.chunk.css"
+  },
+  {
+    "revision": "2e7bef0d843562451f0b",
+    "url": "/static/css/main.44456475.chunk.css"
+  },
+  {
+    "revision": "a436739529af887307e0",
+    "url": "/static/js/2.7270819b.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "/static/js/2.7270819b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e7bef0d843562451f0b",
+    "url": "/static/js/main.4dc13fff.chunk.js"
+  },
+  {
+    "revision": "cad7755f9be513884661",
+    "url": "/static/js/runtime-main.642dcfc8.js"
+  }
+]);
